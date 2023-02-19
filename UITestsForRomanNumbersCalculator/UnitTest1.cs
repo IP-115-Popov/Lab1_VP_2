cd32b361656db8530523f45642656a37d7d5bdda
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-19 19:14:09 +0700

    Add test

--- UITestsForRomanNumbersCalculator/UnitTest1.cs
+++ UITestsForRomanNumbersCalculator/UnitTest1.cs
@@ -1,11 +1,33 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+
 namespace UITestsForRomanNumbersCalculator
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void TestKhaki()
         {
+            var app = AvaloniaApp.GetApp();
+            var mainWindow = AvaloniaApp.GetMainWindow();
 
+            string? c = "I";
+
+            await Task.Delay(100);
+
+            var buttonOne = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonOne");
+
+            var textBox = mainWindow.GetVisualDescendants().OfType<TextBox>().First();
+
+            buttonOne.Command.Execute(buttonOne.CommandParameter);
+
+            await Task.Delay(50);
+
+            var textBoxText = textBox.Text as string;
+
+            Assert.True(textBoxText.Equals(c));
         }
     }
 }
