cebea18d6e6d4728e2231369811323fefc554df7
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-19 19:35:03 +0700

    Add 2 test

--- UITestsForRomanNumbersCalculator/UnitTest1.cs
+++ UITestsForRomanNumbersCalculator/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Media;
 using Avalonia.VisualTree;
 
@@ -8,7 +10,7 @@ namespace UITestsForRomanNumbersCalculator
     public class UnitTest1
     {
         [Fact]
-        public async void TestKhaki()
+        public async void Test1()
         {
             var app = AvaloniaApp.GetApp();
             var mainWindow = AvaloniaApp.GetMainWindow();
@@ -28,6 +30,28 @@ namespace UITestsForRomanNumbersCalculator
             var textBoxText = textBox.Text as string;
 
             Assert.True(textBoxText.Equals(c));
+        }
+        [Fact]
+        public async void Test2()
+        {
+            var app = AvaloniaApp.GetApp();
+            var mainWindow = AvaloniaApp.GetMainWindow();
+
+            string? cc = null;
+
+            await Task.Delay(100);
+
+            var buttonClin = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonClin");
+
+            var textBox = mainWindow.GetVisualDescendants().OfType<TextBox>().First();
+
+            buttonClin.Command.Execute(buttonClin.CommandParameter);
+
+            await Task.Delay(50);
+
+            var textBoxText = textBox.Text as string;
+
+            Assert.True(cc == null);
         }
     }
 }
