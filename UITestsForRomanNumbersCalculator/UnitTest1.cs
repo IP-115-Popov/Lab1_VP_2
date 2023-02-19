1f981eb2798ef8c7892ae4997c173270ceda63d4
IP-115-Popov <serzh.serp123.popov@mail.ru>
2023-02-19 20:11:17 +0700

    Fix test and add 3test

--- UITestsForRomanNumbersCalculator/UnitTest1.cs
+++ UITestsForRomanNumbersCalculator/UnitTest1.cs
@@ -20,16 +20,14 @@ namespace UITestsForRomanNumbersCalculator
             await Task.Delay(100);
 
             var buttonOne = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonOne");
-
             var textBox = mainWindow.GetVisualDescendants().OfType<TextBox>().First();
+            textBox.Text = null;
 
             buttonOne.Command.Execute(buttonOne.CommandParameter);
-
             await Task.Delay(50);
 
             var textBoxText = textBox.Text as string;
-
-            Assert.True(textBoxText.Equals(c));
+            Assert.True(c == textBoxText);
         }
         [Fact]
         public async void Test2()
@@ -52,6 +50,33 @@ namespace UITestsForRomanNumbersCalculator
             var textBoxText = textBox.Text as string;
 
             Assert.True(cc == textBoxText);
+        }
+        [Fact]
+        public async void Test3()
+        {
+            var app = AvaloniaApp.GetApp();
+            var mainWindow = AvaloniaApp.GetMainWindow();
+
+            string? c3 = "CC";
+
+            await Task.Delay(100);
+
+            var buttonHandred = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonHandred");
+            var buttonPlas = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "ButtonPlas");
+            //var button = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "Button");
+
+            var textBox = mainWindow.GetVisualDescendants().OfType<TextBox>().First();
+            textBox.Text = null;
+
+            buttonHandred.Command.Execute(buttonHandred.CommandParameter);
+            await Task.Delay(500);
+            buttonHandred.Command.Execute(buttonHandred.CommandParameter);
+            await Task.Delay(500);
+
+            var textBoxText = textBox.Text as string;
+
+            Assert.True(c3 == textBoxText);
+
         }
     }
 }
